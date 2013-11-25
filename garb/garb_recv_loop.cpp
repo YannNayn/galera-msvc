9d82d7b6d93bc325d94379896ee6f0f4e6f4423d
YannNayn <yann.nayn@yahoo.fr>
2013-11-25 16:56:30 +0000

    Update garb_recv_loop.cpp

--- garb/garb_recv_loop.cpp
+++ garb/garb_recv_loop.cpp
@@ -4,48 +4,7 @@
 
 #include <signal.h>
 #ifdef _MSC_VER
-#include <windows.h>
-/* POSIX emulation layer for Windows.
-*
-* Copyright (C) 2008-2013 Anope Team <team@anope.org>
-*
-* Please read COPYING and README for further details.
-*
-* Based on the original code of Epona by Lara.
-* Based on the original code of Services by Andy Church.
-*/
-#define sigemptyset(x) memset((x), 0, sizeof(*(x)))
-
-#ifndef SIGHUP
-# define SIGHUP -1
-#endif
-#ifndef SIGPIPE
-# define SIGPIPE -1
-#endif
-
-struct sigaction
-{
-    void (*sa_handler)(int);
-    int sa_flags;
-    int sa_mask;
-};
-
-int sigaction(int sig, struct sigaction *action, struct sigaction *old)
-{
-    if (sig == -1)
-        return 0;
-    if (old == NULL)
-    {
-        if (signal(sig, SIG_DFL) == SIG_ERR)
-            return -1;
-    }
-    else
-    {
-        if (signal(sig, action->sa_handler) == SIG_ERR)
-            return -1;
-    }
-    return 0;
-}
+#include <msvc_sup.h>
 #endif
 namespace garb
 {
