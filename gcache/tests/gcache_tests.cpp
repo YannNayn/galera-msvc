48be1dcf55bff53df1fd1ac1f8ef0924bf376cfd
YourName@a.a <you@example.com>
2013-11-25 23:49:08 +0100

    .

--- gcache/tests/gcache_tests.cpp
+++ gcache/tests/gcache_tests.cpp
@@ -12,7 +12,11 @@ extern "C" {
 
 int main(int argc, char* argv[])
 {
+#ifdef _MSC_VER
+    bool  no_fork  = 1;
+#else    
     bool  no_fork  = (argc >= 2 && std::string(argv[1]) == "nofork");
+#endif    
     FILE* log_file = 0;
 
     if (!no_fork)
